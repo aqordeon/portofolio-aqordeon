a1cb3a60afc0722b740a40095b30a9fd85a5aa45
AqordeoN <aqordivika@yahoo.co.id>
2024-04-11 22:38:59 +0800

    fix: remove unused composables

--- composables/useChangeTheme.ts
+++
@@ -1,9 +0,0 @@
-// It will be available as useFoo() (camelCase of file name without extension)
-export const useChangeTheme = (e) => {
-  const themeActive = 'theme-1'
-
-  const changeTheme = (e) => {
-    themeActive = e
-  }
-  return useState('foo', () => console.log(e))
-}
