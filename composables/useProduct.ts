ead255420c539c877ca3b4268ac17192a6ab7b85
AqordeoN <aqordivika@yahoo.co.id>
2025-10-05 21:19:59 +0800

    feat: Products: link online shop

--- composables/useProduct.ts
+++ composables/useProduct.ts
@@ -1,15 +1,5 @@
 export const decks = [
     {
-        slug: 'berani-gak',
-        title: 'Berani Gak?',
-        sku: 'ttk-c-bg-1',
-        imageSrc: '/images/cards/bg1-4x5.webp',
-		imageAlt: "Card variant Berani Gak?",
-        tags: ['fun', 'challenge'],
-        is_topseller: false,
-        occasion: ['nongkrong']
-    },
-    {
         slug: 'deep',
         title: 'Deep',
         sku: 'ttk-c-deep-1',
@@ -17,7 +7,13 @@ export const decks = [
 		imageAlt: "Card variant Deep",
         tags: ['date', 'relationship', 'deep talk'],
         is_topseller: true,
-        occasion: ['deep talk', 'date', 'couple', 'relationship']
+        occasion: ['deep talk', 'date', 'couple', 'relationship'],
+        link_online_shop: {
+            shopee: 'https://shopee.co.id/product/73180818/24179886317',
+            tiktokshop: 'https://vt.tokopedia.com/t/ZSH7ey5FADXMQ-djfKR/',
+            tokopedia: 'https://www.tokopedia.com/tokotangankanan/kartu-edisi-deep-talk-permainan-untuk-pasangan-sahabat-kado-nikah-tanpa-dare-couples-game-1729878628988847821',
+            lazada: 'https://www.lazada.co.id/products/i8231290361.html',
+        }
     },
     {
         slug: 'hangout',
@@ -27,7 +23,13 @@ export const decks = [
 		imageAlt: "Card variant Hangout",
         tags: ['fun', 'challenge'],
         is_topseller: true,
-        occasion: ['nongkrong', 'date']
+        occasion: ['nongkrong', 'date'],
+        link_online_shop: {
+            shopee: 'https://shopee.co.id/product/73180818/24229872610/',
+            tiktokshop: 'https://vt.tokopedia.com/t/ZSH7eyqDFuwPB-mbxTL/',
+            tokopedia: 'https://www.tokopedia.com/tokotangankanan/kartu-truth-edisi-nongkrong-game-seru-untuk-teman-sahabat-permainan-pertanyaan-obrolan-tanpa-dare-hangout-toko-tangan-kanan-1729878736879519437',
+            lazada: 'https://www.lazada.co.id/products/i8231302294.html',
+        }
     },
     {
         slug: 'tot',
@@ -37,17 +39,13 @@ export const decks = [
 		imageAlt: "Card variant This or That",
         tags: ['fun', 'first date'],
         is_topseller: false,
-        occasion: ['nongkrong', 'first date', 'ice breaking']
-    },
-    {
-        slug: 'badboy',
-        title: 'Badboy',
-        sku: 'ttk-c-badboy-1',
-        imageSrc: '/images/cards/badboy1-4x5.webp',
-		imageAlt: "Card variant Bad Boy",
-        tags: ['fun', 'challenge'],
-        is_topseller: false,
-        occasion: ['nongkrong', 'night talk']
+        occasion: ['nongkrong', 'first date', 'ice breaking'],
+        link_online_shop: {
+            shopee: 'https://shopee.co.id/product/73180818/26852148737',
+            tiktokshop: 'https://vt.tokopedia.com/t/ZSH7eyqmao2ES-E5s8O/',
+            tokopedia: 'https://www.tokopedia.com/tokotangankanan/board-games-mirip-kartu-truth-tanpa-dare-this-or-that-edisi-topik-ringan-dan-seru-permainan-1729878837309310669',
+            lazada: 'https://www.lazada.co.id/products/i8231226882.html',
+        }
     },
     {
         slug: 'family99',
@@ -57,7 +55,45 @@ export const decks = [
 		imageAlt: "Card variant Family 99",
         tags: ['fun', 'challenge', 'games'],
         is_topseller: true,
-        occasion: ['nongkrong', 'ice breaking', 'gathering party']
+        occasion: ['nongkrong', 'ice breaking', 'gathering party'],
+        link_online_shop: {
+            shopee: 'https://shopee.co.id/product/73180818/28400024327',
+            tiktokshop: 'https://vt.tokopedia.com/t/ZSH7eystMem7t-RfS0U/',
+            tokopedia: 'https://www.tokopedia.com/tokotangankanan/toko-tangan-kanan-kartu-quiz-mirip-family-100-family-99-board-games-permainan-kartu-family-99-edition-i-cocok-untuk-reuni-sekolah-dan-kumpul-keluarga-card-1729879096179525325',
+            lazada: 'https://www.lazada.co.id/products/i8231226945.html',
+        }
+    },
+    {
+        slug: 'berani-gak',
+        title: 'Berani Gak?',
+        sku: 'ttk-c-bg-1',
+        imageSrc: '/images/cards/bg1-4x5.webp',
+		imageAlt: "Card variant Berani Gak?",
+        tags: ['fun', 'challenge'],
+        is_topseller: false,
+        occasion: ['nongkrong'],
+        link_online_shop: {
+            shopee: 'https://shopee.co.id/product/73180818/25186996457',
+            tiktokshop: 'https://vt.tokopedia.com/t/ZSH7eypSJySyP-aEwb9/',
+            tokopedia: 'https://www.tokopedia.com/tokotangankanan/board-games-kartu-truth-and-dare-dalam-satu-kartu-berani-ga-nongkrong-edition-permainan-1730281694476732109',
+            lazada: 'https://www.lazada.co.id/products/i8271416052.html',
+        }
+    },
+    {
+        slug: 'badboy',
+        title: 'Badboy',
+        sku: 'ttk-c-badboy-1',
+        imageSrc: '/images/cards/badboy1-4x5.webp',
+		imageAlt: "Card variant Bad Boy",
+        tags: ['fun', 'challenge'],
+        is_topseller: false,
+        occasion: ['nongkrong', 'night talk'],
+        link_online_shop: {
+            shopee: 'https://shopee.co.id/product/73180818/40053646045',
+            tiktokshop: 'https://vt.tokopedia.com/t/ZSH7eytoa61U4-crV8W/',
+            tokopedia: 'https://www.tokopedia.com/tokotangankanan/kartu-truth-badboy-edisi-nongkrong-game-card-seru-bareng-sirkel-cowok-tanpa-dare-boys-game-1731618372315809485',
+            lazada: 'https://www.lazada.co.id/products/i8738920035.html',
+        }
     },
     {
         slug: 'tot-2',
@@ -67,7 +103,13 @@ export const decks = [
 		imageAlt: "Card variant This or That 2",
         tags: ['fun', 'first date'],
         is_topseller: true,
-        occasion: ['nongkrong', 'first date', 'ice breaking', 'gathering party']
+        occasion: ['nongkrong', 'first date', 'ice breaking', 'gathering party'],
+        link_online_shop: {
+            shopee: 'https://shopee.co.id/product/73180818/43372292025/',
+            tiktokshop: 'https://vt.tokopedia.com/t/ZSH7eyCGdXLLJ-4eewi/',
+            tokopedia: 'https://www.tokopedia.com/tokotangankanan/toko-tangan-kanan-permainan-kartu-this-or-that-2-pertanyaan-dengan-konteks-pilihan-seru-untuk-teman-nongkrong-bentuk-minimalis-bisa-jadi-topik-obrolan-lebih-menantang-interaktif-card-games-1732749024385205965',
+            lazada: 'https://www.lazada.co.id/products/i8738900654.html',
+        }
     },
 ]
 
