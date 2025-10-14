61e8273508a6bcf5fed67554ddc9494bcafc482d
AqordeoN <aqordivika@yahoo.co.id>
2025-10-14 22:51:38 +0800

    feat: Header: navbar semua kartu and logo

--- components/Layouts/HeaderTTK.vue
+++ components/Layouts/HeaderTTK.vue
@@ -1,6 +1,6 @@
 <template>
     <header class="bg-white sticky inset-x-0 top-0 z-50">
-        <nav class="mx-auto flex xmax-w-7xl items-center justify-between p-4 lg:px-8" aria-label="Global">
+        <nav class="mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-8" aria-label="Global">
             <div class="flex lg:flex-1">
                 <NuxtLink href="/" class="-m-1.5 p-1.5">
                     <span class="sr-only">Toko Tangan Kanan</span>
@@ -8,6 +8,8 @@
                         src="/ttk/ttk-teal-logo.png" alt="" />
                 </NuxtLink>
             </div>
+            
+            <!-- Button: Hamburger (mobile) -->
             <div class="flex lg:hidden">
                 <button type="button"
                     class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
@@ -16,14 +18,15 @@
                     <Bars3Icon class="size-6" aria-hidden="true" />
                 </button>
             </div>
+
+            <!-- Section: list navbar (desktop) -->
             <div class="hidden lg:flex lg:gap-x-12">
-                <NuxtLink v-for="item in navigation" :key="item.name" :href="item.href"
-                    class="text-sm/6 font-semibold text-gray-900">{{ item.name }}</NuxtLink>
+                <div @click="_popover?.toggle" class="text-sm/6 font-semibold ">
+                    Semua kartu
+                </div>
+                <NuxtLink href="/tentang-kami" class="text-sm/6 font-semibold ">Tentang kami</NuxtLink>
+                <NuxtLink href="/kontak" class="text-sm/6 font-semibold ">Kontak</NuxtLink>
             </div>
-            <!-- <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-                <a href="#" class="text-sm/6 font-semibold text-gray-900">Log in <span
-                        aria-hidden="true">&rarr;</span></a>
-            </div> -->
         </nav>
 
         <Dialog class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
@@ -45,19 +48,37 @@
                 <div class="mt-6 flow-root">
                     <div class="-my-6 divide-y divide-gray-500/10">
                         <div class="space-y-2 py-6">
-                            <a v-for="item in navigation" :key="item.name" :href="item.href"
-                                class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">{{
+                            <a v-for="item in cardsList" :key="item.name" :href="item.href"
+                                class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold  hover:bg-gray-50">{{
                                 item.name }}</a>
                         </div>
                         <div class="py-6">
                             <a href="#"
-                                class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Log
+                                class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold  hover:bg-gray-50">Log
                                 in</a>
                         </div>
                     </div>
                 </div>
             </DialogPanel>
         </Dialog>
+
+        <!-- Popover: semua kartu -->
+        <Popover ref="_popover">
+            <div class="flex flex-col py-1 gap-4 w-[15rem]">
+                <ul class="list-none p-0 m-0 flex flex-col gap-2">
+                    <NuxtLink v-for="card, idxCard in cardsList" :key="idxCard" :href="card.href" class="hover:bg-primary/10 px-3 py-1 rounded flex items-center gap-2">
+                        <div>
+                            <span class="font-medium">{{ card.name }}</span>
+                        </div>
+                        <div class="ml-auto h-fit">
+                            <div class="aspect-[16/9] w-10">
+                                <img :src="card.img" :alt="card.name" class="object-contain h-full w-full" />
+                            </div>
+                        </div>
+                    </NuxtLink>
+                </ul>
+            </div>
+        </Popover>
     </header>
 </template>
 
@@ -65,18 +86,49 @@
 
 import { Dialog, DialogPanel } from '@headlessui/vue'
 import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
+import { Popover } from 'primevue'
 
-const navigation = [
-	{ name: 'Semua kartu', href: '/cards' },
-	{ name: 'Deep', href: '#' },
-	{ name: 'Hangout', href: '#' },
-	{ name: 'This or That', href: '#' },
-	{ name: 'Family99', href: '#' },
-	{ name: 'Badboy', href: '#' },
-	{ name: 'Berani Gak?', href: '#' },
+const cardsList = [
+	{
+        name: 'Deep',
+        img: '/images/logo/deep_logo.png',
+        href: '/cards/deep'
+    },
+	{
+        name: 'Hangout',
+        img: '/images/logo/hangout_logo.png',
+        href: '/cards/hangout'
+    },
+	{
+        name: 'This or That',
+        img: '/images/logo/tot_logo.png',
+        href: '/cards/tot'
+    },
+	{
+        name: 'Family99',
+        img: '/images/logo/family99_logo.png',
+        href: '/cards/family99'
+    },
+	{
+        name: 'Badboy',
+        img: '/images/logo/badboy_logo.png',
+        href: '/cards/badboy'
+    },
+	{
+        name: 'Berani Gak?',
+        img: '/images/logo/beranigak_logo.png',
+        href: '/cards/berani_gak'
+    },
+	{
+        name: 'This or That 2',
+        img: '/images/logo/tot2_logo.png',
+        href: '/cards/tot2'
+    },
 ]
 
 const mobileMenuOpen = ref(false)
+
+const _popover = ref(null)
 </script>
 
 <style></style>
