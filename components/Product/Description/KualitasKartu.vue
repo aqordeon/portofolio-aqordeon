a611d2286cd30215cd704516e095533ac4ae8efc
AqordeoN <aqordivika@yahoo.co.id>
2026-02-02 23:15:44 +0800

    fix: KualitasKartu: improve text clarity and formatting

--- components/Product/Description/KualitasKartu.vue
+++ components/Product/Description/KualitasKartu.vue
@@ -4,16 +4,16 @@
 
 <template>
     <div>
-        <p> Kualitas kartu: </p>
+        <p>Kualitas kartu: </p>
         <ul class="list-inside list-disc">
-            <!-- <li> Desain aesthetic (instagramable). </li> -->
-            <li>Desain sangat menarik (instagramable). </li>
+            <li>Desain sangat menarik dan modern (aesthetic untuk dipost ke sosmed). </li>
             <li>Terdapat 2 versi bahasa sekaligus, Indonesia dan English.</li>
             <li>Ringan dan portabel.</li>
-            <li> Hasil cetakan kartu sangat jelas, dapat terbaca dengan baik. </li>
+            <li>Hasil cetakan kartu sangat jelas, tajam, dan dapat terbaca dengan baik. </li>
             <li>
                 Kartu difinishing doff, semi anti-air, dan rounded corner.
             </li>
+            <li>Rounded corner sehingga nyaman ketika dimainkan.</li>
         </ul>
     </div>
 </template>
